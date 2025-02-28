1b7b5f16ae27fcadd4ad682b3c0e42fa6bd761e5
dotunv <victoroluwasomidotun@gmail.com>
2025-03-01 00:03:15 +0100

    Add Django admin static files and configuration
    - Added comprehensive Django admin static files (CSS, JS, images)
    - Updated settings with login and logout redirect configurations
    - Removed deprecated production settings file
    - Prepared admin interface resources for the project

--- Quajo/settings.py
+++ Quajo/settings.py
@@ -44,9 +44,10 @@ INSTALLED_APPS = [
     'dashboard'
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,7 +122,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
