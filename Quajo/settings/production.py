09b6944d81d9b953d3f8fa80f65c5a2842e28ea5
dotunv <victoroluwasomidotun@gmail.com>
2025-03-01 00:17:42 +0100

    Update production allowed hosts for Render deployment
    - Added 'quajo.onrender.com' to ALLOWED_HOSTS
    - Ensures proper access to the production application on Render

--- Quajo/settings/production.py
+++ Quajo/settings/production.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1', 'quajo.onrender.com']
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
